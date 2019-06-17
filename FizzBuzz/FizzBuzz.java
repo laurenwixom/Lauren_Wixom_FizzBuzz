9899dc9ed9b4425a18869cf9d087c667e34be93a
Lauren Wixom <50384167+laurenwixom@users.noreply.github.com>
2019-06-16 21:03:51 -0500

    Add files via upload

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,24 @@
+public class FizzBuzz {
+    public String fizzBuzz(int n) 
+      {
+
+		if(n % 3 == 0 && n % 5 ==0) 
+		  {
+			 System.out.println("FizzBuzz");
+		  }
+
+		else if(n % 3 == 0) 
+		  {
+    		System.out.println("Fizz");
+          }
+
+    	else if(n % 5 == 0 ) 
+    	{
+    		System.out.println("Buzz");
+		}
+
+		else 
+		}
+			System.out.println(n);
+		}
+	
